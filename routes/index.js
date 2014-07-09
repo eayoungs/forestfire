fb9b011f150efe92912ae8cac2529c58ce3b192e
Don Park <donp@donp.org>
2014-07-09 09:24:51 -0700

    Wildfile is one word

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Wild Fire Watch' });
+  res.render('index', { title: 'Wildfire Watch' });
 });
 
 module.exports = router;
