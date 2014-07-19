efc5d96d80ae3bf53bbfa9bd615dd38574ffa767
Don Park <donp@donp.org>
2014-07-19 10:55:40 -0700

    load code, load fires

--- public/javascripts/code.js
+++ public/javascripts/code.js
@@ -1,1 +1,20 @@
-console.log("code.js loaded")
+var forestfire = (function(){
+  var fire = {}
+  var fires = {}
+
+  fire.setup = function(){
+    jQuery.ajax('/fires.json', {success:fire.loaded})
+  }
+
+  fire.loaded = function(data, status){
+    fires = data
+  }
+
+  fire.nearby = function(){
+    console.log(fires)
+  }
+
+  return fire
+})()
+
+
