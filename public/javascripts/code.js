fa16fc4cb6799facc893be1be180e4610d2c9a23
Don Park <donp@donp.org>
2014-07-19 11:37:41 -0700

    introduce dust client-side templates

--- public/javascripts/code.js
+++ public/javascripts/code.js
@@ -1,18 +1,24 @@
 var forestfire = (function(){
+  // the code container
   var fire = {}
+
+  // latest fire data
   var fires = {}
 
-  fire.setup = function(){
-    jQuery.ajax('/fires.json', {success:fire.loaded})
+  fire.setup = function(callback){
+    jQuery.ajax('/fires.json', {success:function(data, status){fire.loaded(data,status,callback)}})
   }
 
-  fire.loaded = function(data, status){
+  fire.loaded = function(data, status, callback){
     fires = data
+    callback()
   }
 
   fire.nearby = function(){
     console.log(fires)
   }
+
+  fire.fires = function(){ return fires}
 
   return fire
 })()
